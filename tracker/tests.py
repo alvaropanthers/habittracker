b57e0d34e58c8cd92e884ec610a92b43d335e6c5
Alvaro Orozco <aoroz032@fiu.edu>
2020-04-04 16:22:00 -0400

    TTD

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,3 +1,70 @@
 from django.test import TestCase
+from . import utils
 
-# Create your tests here.
+class UtilTestCases(TestCase):
+    def test_break_formatted_date(self):
+        self.assertEqual(utils.break_formatted_date('11-20-1995'), [11, 20, 1995])
+        self.assertEqual(utils.break_formatted_date('01-01-1995'), [1, 1, 1995])
+        self.assertEqual(utils.break_formatted_date('1-1-1995-20'), [])
+
+    def test_format_date(self):
+        self.assertEqual(utils.format_date(11, 20, 1995), '11-20-1995')
+        self.assertEqual(utils.format_date(1, 20, 1995), '01-20-1995')
+        with self.assertRaises(ValueError):
+            utils.format_date(1, 20, 19)
+
+        self.assertEqual(utils.format_date(0, 20, 2020), '01-20-2020')
+        self.assertEqual(utils.format_date(0, 0, 2020), '01-01-2020')
+
+    def test_is_month(self):
+        for index in range(1, 13):
+            self.assertTrue(utils.is_month(index))
+
+        self.assertTrue(utils.is_month('11'))
+        self.assertTrue(utils.is_month('1'))
+        self.assertTrue(utils.is_month('01'))
+        self.assertTrue(utils.is_month('000001'))
+        self.assertFalse(utils.is_month('100'))
+        self.assertFalse(utils.is_month('0'))
+        self.assertFalse(utils.is_month('13'))
+        self.assertFalse(utils.is_month('string'))
+
+    def test_is_day(self):
+        for index in range(1, 32):
+            self.assertTrue(utils.is_day(index))
+
+        self.assertTrue(utils.is_day('11'))
+        self.assertTrue(utils.is_day('1'))
+        self.assertTrue(utils.is_day('01'))
+        self.assertTrue(utils.is_day('000001'))
+        self.assertFalse(utils.is_day('100'))
+        self.assertFalse(utils.is_day('0'))
+        self.assertFalse(utils.is_day('32'))
+        self.assertFalse(utils.is_day('string'))
+
+    def test_is_year(self):
+        for index in range(2000, 2021):
+            self.assertTrue(utils.is_year(index))
+        
+        self.assertTrue(utils.is_year('2000'))
+        self.assertTrue(utils.is_year('0002000'))
+        self.assertFalse(utils.is_year('200'))
+        self.assertFalse(utils.is_year('0200'))
+        self.assertFalse(utils.is_year('string'))
+        self.assertFalse(utils.is_year('1995'))
+
+    def test_is_date(self):
+        self.assertTrue(utils.is_date('11-20-2020'))
+        self.assertFalse(utils.is_date('11-20-1995'))
+        self.assertFalse(utils.is_date('11-20-1995-10-100'))
+        self.assertTrue(utils.is_date('01-20-2020'))
+        self.assertTrue(utils.is_date('01-09-2020'))
+
+        for index in range(1, 13):
+            self.assertTrue(utils.is_date(f'{index}-{index}-2020'))
+
+class ViewTestClases(TestCase):
+    from . import views
+    def test_get_all_templates(self):
+        pass
+
--- tracker/utils.py
+++ tracker/utils.py
@@ -1,23 +1,55 @@
 from django.utils import timezone
 
-def break_formatted_date(date):
-    return list(map(int, date.split('-')))
+_MIN_YEAR = 2000
 
-def format_date(month, day, year):
+def break_formatted_date(date):
+    items = list(map(int, date.split('-')))
+    return items if len(items) == 3 else []
+
+def format_date(month, day, year): #value error if a string is passed
+    month = int(month)
+    day = int(day)
+    year = int(year)
+
+    if  0 < month < 10:
+        month = f'0{month}'
+    elif month == 0:
+        month = '01' 
+    
+    if 0 < day < 10:
+        day = f'0{day}'
+    elif day == 0:
+        day = '01'
+
+    if not (1995 <= year <= 2020):
+        raise ValueError
+
     return f"{month}-{day}-{year}"
 
 def is_month(month):
-    return is_integer(month) and 1 <= month <= 12
+    try:
+        return 1 <= int(month) <= 12
+    except ValueError:
+        return False
 
 def is_day(day):
-    return is_integer(month) and 1 <= month <= 31
+    try:
+        return 1 <= int(day) <= 31
+    except ValueError:
+        return False
 
 def is_year(year):
-    return is_integer(year) and 2000 <= year <= timezone.now().year
+    try:
+        return _MIN_YEAR <= int(year) <= timezone.now().year
+    except ValueError:
+        return False
 
 def is_date(date):
-    items = break_formatted_date(date)
-    return len(date) == 3 and is_month(items[0]) and is_day(items[1]) and is_year(items[2])
+    try:
+        month, day, year = break_formatted_date(date)
+        return is_month(month) and is_day(day) and is_year(year)
+    except ValueError:
+        return False
 
 def is_integer(value):
     return type(value) == int
